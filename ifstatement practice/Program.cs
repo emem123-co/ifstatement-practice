9fc1f0ebccad583f3e7cabdf3649d345ed1f2af8
Emily Morgan <emorgan921@gmail.com>
2024-06-12 14:30:48 -0400

    Add project files.

---
+++ ifstatement practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+using System.Net.Security;
+
+namespace ifstatement_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            string answer = Console.ReadLine();
+
+            int aNumber = Convert.ToInt32(answer);
+
+            if (aNumber % 2 == 0)
+            {Console.WriteLine($"{aNumber} is even.");}
+            else{Console.WriteLine($"{aNumber} is odd.");}
+
+            
+        }
+    }
+}
